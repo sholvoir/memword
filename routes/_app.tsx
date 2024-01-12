2d871406523bb8830c167def2e4063d21227d06e
sholvoir <sovar.he@gmail.com>
2024-01-12 11:31:42 -0500

    capable

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -6,7 +6,7 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
         <title>MemWord</title>
         <link rel="icon" href="/favicon.svg" />
         <link rel="apple-touch-icon" sizes="57x57" href="/icon/icon-57.png"/>
