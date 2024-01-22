8f05958f47447a95a68d3477a33e6f677cca3c1d
sholvoir <sovar.he@gmail.com>
2024-01-22 09:56:51 -0500

    bigger button

--- islands/study.tsx
+++ islands/study.tsx
@@ -96,10 +96,10 @@ export default ({ studies, showTips, onFinish }: StudyProps) => {
             {isPhaseAnswer.value && <div><pre>{study.value.trans}</pre></div>}
         </div>
         <audio ref={player} src={shouldSound.value ? study.value.sound : undefined} autoplay/>
-        <div class="absolute bottom-1/3 right-2 flex flex-col gap-1">
-            <NButton class="grow" onClick={handleShowAnswer} title="_" disabled={isPhaseAnswer.value}>Answer</NButton>
-            <NButton class="grow" onClick={handleIKnown} title="X/N" disabled={!isPhaseAnswer.value}>Known</NButton>
-            <NButton class="grow" onClick={handleDontKnow} title="Z/M" disabled={!isPhaseAnswer.value}>Don't</NButton>
+        <div class="absolute bottom-1/3 right-2 flex flex-col gap-2 text-lg">
+            <NButton class="grow py-3" onClick={handleShowAnswer} title="_" disabled={isPhaseAnswer.value}>Answer</NButton>
+            <NButton class="grow py-3" onClick={handleIKnown} title="X/N" disabled={!isPhaseAnswer.value}>Known</NButton>
+            <NButton class="grow py-3" onClick={handleDontKnow} title="Z/M" disabled={!isPhaseAnswer.value}>Don't</NButton>
         </div>
     </div>;
 }
