bc9917693eb60c71d95b4d77cb602d1e6b40152c
sholvoir <sovar.he@gmail.com>
2024-02-05 18:14:55 -0500

    try full screen

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -4,9 +4,10 @@ export default function App({ Component }: PageProps) {
   return (
     <html lang="en">
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta charSet="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <link rel="manifest" href="/manifest.json"/>
+        <meta name="apple-mobile-web-app-capable" content="yes"/>
         <title>MemWord</title>
         <link rel="icon" href="/favicon-light.svg" media="(prefers-color-scheme: light)"/>
         <link rel="icon" href="/favicon-dark.svg" media="(prefers-color-scheme: dark)"/>
@@ -18,11 +19,11 @@ export default function App({ Component }: PageProps) {
         <link rel="apple-touch-icon" sizes="144x144" href="/icon/icon-144.png"/>
         <link rel="apple-touch-icon" sizes="152x152" href="/icon/icon-152.png"/>
         <link rel="apple-touch-icon" sizes="180x180" href="/icon/icon-180.png"/>
-        <link rel="stylesheet" href="/styles.css" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;600;700&display=swap" />
+        <link rel="stylesheet" href="/styles.css"/>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;600;700&display=swap"/>
       </head>
-      <body class="h-[100dvh] bg-slate-200 text-slate-800 dark:bg-slate-800 dark:text-slate-300" style="font-family: 'Roboto', sans-serif;">
-        <Component />
+      <body class="h-[100lvh] bg-slate-200 text-slate-800 dark:bg-slate-800 dark:text-slate-300" style="font-family: 'Roboto', sans-serif;">
+        <Component/>
       </body>
     </html>
   );
