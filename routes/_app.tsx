7c99ab4aeffee5c247080c6b545c473d32dbec03
sholvoir <sovar.he@gmail.com>
2024-02-05 18:58:39 -0500

    actions/checkout@v4

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -7,8 +7,7 @@ export default function App({ Component }: PageProps) {
         <meta charSet="utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <meta name="apple-mobile-web-app-capable" content="yes"/>
-        <meta name="apple-mobile-web-app-status-bar-style" content="white" media="(prefers-color-scheme: light)"/>
-        <meta name="apple-mobile-web-app-status-bar-style" content="black" media="(prefers-color-scheme: dark)"/>
+        <meta name="apple-mobile-web-app-status-bar-style" content="black"/>
         <title>MemWord</title>
         <link rel="icon" href="/favicon-light.svg" media="(prefers-color-scheme: light)"/>
         <link rel="icon" href="/favicon-dark.svg" media="(prefers-color-scheme: dark)"/>
