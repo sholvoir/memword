95394293bef887d8035ba42201b1838ea0306ceb
sholvoir <sovar.he@gmail.com>
2024-02-07 11:05:31 -0500

    icon V

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -7,11 +7,14 @@ export default function App({ Component }: PageProps) {
         <meta charSet="utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <title>MemWord</title>
-        <link rel="icon" type="image/png" sizes="48x48" href="/icon/icon-48.png"/>
+        <link rel="icon" type="image/png" sizes="192x192" href="/icon/icon-192.png"/>
         <link rel="manifest" href="/manifest.json" />
         <meta name="apple-mobile-web-app-capable" content="yes"/>
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"/>
-        <link rel="apple-touch-icon" sizes="192x192" href="/icon/icon-192.png"></link>
+        <link rel="apple-touch-icon" sizes="120x120" href="/icon/icon-120.png"></link>
+        <link rel="apple-touch-icon" sizes="180x180" href="/icon/icon-180.png"></link>
+        <link rel="apple-touch-icon" sizes="512x512" href="/icon/icon-512.png"></link>
+        <link rel="apple-touch-icon" sizes="1024x1024" href="/icon/icon-1024.png"></link>
         <meta name="theme-color" content="#CBD5E1" media="(prefers-color-scheme: light)"/>
         <meta name="theme-color" content="#0F172A" media="(prefers-color-scheme: dark)"/>
         <link rel="stylesheet" href="/styles.css"/>
