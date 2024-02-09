6e95e46381309e2e999c2dab6875c3684dfab0fa
sholvoir <sovar.he@gmail.com>
2024-02-09 11:28:40 -0500

    stats

--- islands/stats.tsx
+++ islands/stats.tsx
@@ -19,8 +19,8 @@ export default ({ setting, stats, onClickStatBar}: IStatsProps) => {
             const stat = stats.value[taskType][tag];
             const all = BLevels.reduce((s,b) => s + stat.all[b], 0);
             if (setting.value.wordBooks.includes(`${taskType}${tag}`)) {
-                result.push(<div class="grow min-w-80 grid gap-x-1 grid-cols-[max-content_1fr_max-content] items-center">
-                    <div class="col-span-3 text-center font-bold">
+                result.push(<div class="grow min-w-80 grid gap-x-1 grid-cols-[max-content_1fr] items-center">
+                    <div class="col-span-2 text-center font-bold">
                         <a class="hover:cursor-pointer hover:underline" onClick={() => onClickStatBar(taskType, tag)}>{TaskTypeName[taskType]}-{TagName[tag]} - {all}</a>
                     </div>
                     {BLevels.map(blevel => {
@@ -28,14 +28,12 @@ export default ({ setting, stats, onClickStatBar}: IStatsProps) => {
                         const task = stat.task[blevel];
                         return <>
                             <div class="text-left">{BLevelName[blevel]}</div>
-                            <div onClick={(ev: Event) => (ev.stopPropagation(), onClickStatBar(taskType, tag, blevel))}>
-                                <div class="bg-slate-300 dark:bg-slate-700 h-6 py-1 w-full hover:cursor-pointer">
-                                    <div class="my-auto h-4 bg-slate-400" style={`width: ${value * 100 / all}%`}>
-                                        <div class="ml-auto h-full bg-orange-500" style={`width: ${value ? (task * 100 / value) : 0}%`}/>
-                                    </div>
+                            <div class="relative bg-slate-300 dark:bg-slate-700 h-6 py-1 w-full hover:cursor-pointer" onClick={(ev: Event) => (ev.stopPropagation(), onClickStatBar(taskType, tag, blevel))}>
+                                <div class="my-auto h-4 bg-slate-400" style={`width: ${value * 100 / all}%`}>
+                                    <div class="ml-auto h-full bg-orange-500" style={`width: ${value ? (task * 100 / value) : 0}%`}/>
                                 </div>
+                                <div class="absolute top-0 right-0">{task}|{value}</div>
                             </div>
-                            <div class="text-right">{task}|{value}</div>
                         </>
                     })}
                 </div>);
