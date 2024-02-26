efd5de636a6def6e6e29b1452155900829fb0ab5
sholvoir <sovar.he@gmail.com>
2024-02-26 15:10:33 -0500

    remove previous & next

--- islands/help.tsx
+++ islands/help.tsx
@@ -2,12 +2,12 @@ import Dialog from './dialog.tsx';
 import IconCut from "tabler_icons/cut.tsx";
 import IconRefresh from "tabler_icons/refresh.tsx";
 import IconAlertCircleFilled from "tabler_icons/alert-circle-filled.tsx";
-import IconCircleCaretRight from "tabler_icons/circle-caret-right.tsx";
+import IconPlayerPlayFilled from "tabler_icons/player-play-filled.tsx";
 import IconCircleLetterF from "tabler_icons/circle-letter-f.tsx";
 
 export default () => <Dialog title="帮助"><ol class="list-decimal py-2 pr-2 pl-8 [&>li]:mx-2">
     <li>如何使用本软件？
-        <br/>答：点击「学习」，开始学习即可；在学习界面，会显示一个单词，或播放一小段声音，取决于你正在练习听力还是阅读，在心里默想一个概念（意思），点击「答案」，检查是否和自己默想的答案一致，如果一致，点击「知道」，否则点击「不会」。记住，要快速完成，不要在一个词上长时间停留，那样只会降低效率，我们的方法是快速/多次，从记忆效率来讲，次数的作用远远大于时长。这个方法称为大脑按摩，直接在拼写/声音和概念之间建立反射弧，所以不要犹豫，快速完成一个小冲刺。
+        <br/>答：点击「学习」，开始学习即可；在学习界面，(1)当练习听力时，会先播放一小段声音，没有听清可以点击<IconPlayerPlayFilled class="w-6 h-6 inline-block"/>或按下「B或C」快捷键或触摸屏幕的上半部分进行重听，(2)当练习阅读时，会先显示一个单词；然后在心里默想一个概念（意思），点击「答」或按下「空格键」或触摸屏幕下半区域，显示单词的音标和解释，并同时显示一个与该词相关的背景图片，检查是否和自己默想的答案一致，如果一致，点击「知」或按下「N或X」快捷键或向上滑动屏幕，否则点击「不」或按下「M或Z」快捷键或向下滑动屏幕。记住，要快速完成，不要在一个词上长时间停留，那样只会降低效率，我们的方法是快速/多次，从记忆效率来讲，次数的作用远远大于时长。这个方法称为大脑按摩，直接在拼写/声音和概念之间建立反射弧，所以不要犹豫，快速完成一个小冲刺。
     </li>
     <li>我应该学习哪些词汇？
         <br/>答：我们的目标是在未来能够自由使用英语，应该先学重要的词汇，因为20%的英语词汇表达了80%的意思，抓住少数基本词，将能够快速提高英语水平。本软件提供多本词书供您选择，以适应您当下的需求。但不要太多关注词书，一个常用词通常会出现在多本词书中，不必介意它到底从哪里来。也可以不选择词书，直接基于词典（本词典收录25000多条常用词，不在词典中的词，对于学生而言，可以不必记忆了），在词典中查到自己在生活中遇到，但不会的词，直接开始学它，背它，终有一日，你再也遇不到生词的时候，你就已经功法大成了。
@@ -16,7 +16,7 @@ export default () => <Dialog title="帮助"><ol class="list-decimal py-2 pr-2 pl
         <br/>答：完全不必操心这个事情，让系统决定，系统自动根据「艾宾浩斯」遗忘曲线来决定此时的最佳复习内容，你要做的事是每天利用碎片时间，拿出手机，开始「学习」。
     </li>
     <li>界面上的按钮是什么意思？
-        <br/><IconCircleCaretRight class="w-6 h-6 inline-block"/>: 再播一遍声音；
+        <br/><IconPlayerPlayFilled class="w-6 h-6 inline-block"/>: 再播一遍声音；
         <br/><IconCircleLetterF class="w-6 h-6 inline-block"/>: 这个词我已经掌握，直接标记为「已完成」；
         <br/><IconCut class="w-6 h-6 inline-block"/>: 这个词是生僻词，从我的任务列表中删除；
         <br/><IconAlertCircleFilled class="w-6 h-6 inline-block"/>: 这个词的翻译/声音/音标，有问题/错误，请人工处理。
--- islands/study.tsx
+++ islands/study.tsx
@@ -1,4 +1,3 @@
-// deno-lint-ignore-file no-explicit-any
 import { useRef } from "preact/hooks";
 import { useSignal, useComputed } from "@preact/signals";
 import { signals, closeDialog, updateStats, syncTasks, study, getDiction, submitIssue, showTips, removeTask, showDialog } from '../lib/mem.ts';
@@ -7,23 +6,16 @@ import IconRefresh from "tabler_icons/refresh.tsx";
 import IconAlertCircleFilled from "tabler_icons/alert-circle-filled.tsx";
 import IconPlayerPlayFilled from "tabler_icons/player-play-filled.tsx";
 import IconCircleLetterF from "tabler_icons/circle-letter-f.tsx";
-import IconChevronsRight from "tabler_icons/chevrons-right.tsx";
-import IconChevronsLeft from "tabler_icons/chevrons-left.tsx";
 import IconBook2 from "tabler_icons/book-2.tsx";
 import SButton from './button-anti-shake.tsx';
 import Dialog from './dialog.tsx';
 
-let startX = 0;
 let startY = 0;
-let endX = 0;
 let endY = 0;
 
 export default () => {
     const index = useSignal(0);
     const current = useSignal(signals.studies.value[index.value]);
-    const shouldSound = useComputed(() => signals.isPhaseAnswer.value || current.value.type == 'L');
-    const shouldSpell = useComputed(() => signals.isPhaseAnswer.value || current.value.type == 'R');
-
     const finish = () => {
         closeDialog();
         signals.stats.value = { ...signals.stats.value };
@@ -31,51 +23,30 @@ export default () => {
         syncTasks();
     }
     if (!current.value) return (finish(), <div/>);
+    const shouldSound = useComputed(() => signals.isPhaseAnswer.value || current.value.type == 'L');
+    const shouldSpell = useComputed(() => signals.isPhaseAnswer.value || current.value.type == 'R');
     const player = useRef<HTMLAudioElement>(null);
+    const handleIKnown = async () => {
+        await study(current.value, signals.stats.value);
+        signals.isPhaseAnswer.value = false;
+        if (index.value >= signals.studies.value.length) return finish();
+        current.value = signals.studies.value[++index.value];
+    };
     const handleKeyPress = (event: KeyboardEvent ) => {
         if (event.isComposing) return;
         switch (event.code) {
-            case 'Comma': handlePrevious(); break;
-            case 'Period': handleNext(); break;
             case 'KeyB': case 'KeyC': handleSpeakIt(); break;
             case 'Space': if (!signals.isPhaseAnswer.value) handleShowAnswer(); break;
             case 'KeyN': case 'KeyX': if (signals.isPhaseAnswer.value) handleIKnown(); break;
             case 'KeyM': case 'KeyZ': if (signals.isPhaseAnswer.value) handleDontKnow(); break;
         }
     };
-    const handleSpeakIt = () => {
-        if (current.value.sound && shouldSound.value) player.current?.play();
-    };
-    const handleShowAnswer = () => signals.isPhaseAnswer.value = true;
-    const handleIKnown = async () => {
-        await study(current.value, signals.stats.value);
-        handleNext();
-    };
-    const handleDictMaintain = () => {
-        showDialog({dial: 'dictm', study: current})
-    }
-    const handleSkilled = async () => {
-        current.value.level = 14;
-        await handleIKnown();
-    }
-    const handleDontKnow = () => {
-        current.value.level = 0;
-        handleIKnown();
-    };
-    const handleNext = () => {
-        signals.isPhaseAnswer.value = false;
-        if (index.value >= signals.studies.value.length) return finish();
-        current.value = signals.studies.value[++index.value];
-    };
-    const handlePrevious = () => {
-        signals.isPhaseAnswer.value = false;
-        if (index.value <= 0) return finish();
-        current.value = signals.studies.value[--index.value];
-    };
-    const handleRefresh = async () => {
-        const dict: any = await getDiction(current.value.word, true);
-        current.value = { ...current.value, ...dict };
-    };
+    const handleSpeakIt = () => current.value.sound && shouldSound.value && player.current?.play();
+    const handleShowAnswer = () => signals.isPhaseAnswer.value || (signals.isPhaseAnswer.value = true);
+    const handleFinished = () => (current.value.level = 14, handleIKnown());
+    const handleDontKnow = () => (current.value.level = 0, handleIKnown());
+    const handleDictMaintain = () => showDialog({dial: 'dictm', study: current});
+    const handleRefresh = async () => current.value = { ...current.value, ...await getDiction(current.value.word, true) };
     const handleReportIssue = async () => {
         const resp = await submitIssue(current.value.word);
         if (!resp.ok) showTips(await resp.text());
@@ -87,66 +58,38 @@ export default () => {
         current.value = signals.studies.value[index.value];
         signals.isPhaseAnswer.value = false;
     };
-    const handleTouchStart = (e: TouchEvent) => {
-        endX = startX = e.touches[0].clientX;
-        endY = startY = e.touches[0].clientY;
-    };
-    const handleTouchMove = (e: TouchEvent) => {
-        const div = e.currentTarget as HTMLDivElement;
-        endX = e.touches[0].clientX;
-        endY = e.touches[0].clientY;
-        if (Math.abs(endX - startX) > Math.abs(endY - startY)) {
-            div.style.left = `${endX - startX}px`;
-            div.style.top = '0';
-        } else if (signals.isPhaseAnswer.value) {
-            div.style.left = '0';
-            div.style.top = `${endY - startY}px`;
-        }
-    };
-    const moveDivHThenRun = (div: HTMLDivElement, x: number, handle: () => void) => {
-        endX += x;
-        if (endX - startX > div.clientWidth || endX - startX < -div.clientWidth) (handle(), setTimeout(() => {div.style.left = '0'}, 10));
-        else (div.style.left = `${endX - startX}px`, setTimeout(moveDivHThenRun, 20, div, x, handle));
-    };
-    const moveDivVThenRun = (div: HTMLDivElement, y: number, handle: () => void) => {
+    const handleTouchStart = (e: TouchEvent) => endY = startY = e.touches[0].clientY;
+    const handleTouchMove = (e: TouchEvent) => signals.isPhaseAnswer.value && ((e.currentTarget as HTMLDivElement).style.top = `${(endY = e.touches[0].clientY) - startY}px`);
+    const moveDivThenRun = (div: HTMLDivElement, y: number, handle: () => void) => {
         endY += y;
         if (endY - startY > div.clientHeight || endY - startY < -div.clientHeight) (handle(), setTimeout(() => {div.style.top = '0'}, 10));
-        else (div.style.top = `${endY - startY}px`, setTimeout(moveDivVThenRun, 20, div, y, handle));
+        else (div.style.top = `${endY - startY}px`, setTimeout(moveDivThenRun, 20, div, y, handle));
     }
     const handleTouchEnd = (e: Event) => {
         const div = e.currentTarget as HTMLDivElement;
-        if (Math.abs(endX - startX) < 1 && Math.abs(endY - startY) < 1) {
-            console.log('TouchEnd', startX, startY, endX, endY);
+        if (Math.abs(endY - startY) < 1) {
             div.style.top = '0';
-            div.style.left = '0';
             const rect = div.getBoundingClientRect();
             if (startY > rect.top + rect.height / 2) handleShowAnswer();
             else if (startY > rect.top + 36) handleSpeakIt();
-        } else if (Math.abs(endX - startX) > Math.abs(endY - startY)) {
-            if (endX - startX >= div.clientWidth / 2) moveDivHThenRun(div, 20, handlePrevious);
-            else if (endX - startX <= -div.clientWidth / 2) moveDivHThenRun(div, -20, handleNext);
-            else div.style.left = '0';
         } else if (signals.isPhaseAnswer.value) {
-            if (endY - startY >= div.clientHeight / 4) moveDivVThenRun(div, 40, handleDontKnow);
-            else if (endY - startY <= -div.clientHeight / 4) moveDivVThenRun(div, -40, handleIKnown);
+            if (endY - startY >= div.clientHeight / 6) moveDivThenRun(div, 50, handleDontKnow);
+            else if (endY - startY <= -div.clientHeight / 6) moveDivThenRun(div, -50, handleIKnown);
             else div.style.top = '0';
         }
     };
-    const handleTouchCancel = (e: TouchEvent) => {
-        const div = e.currentTarget as HTMLDivElement;
-        div.style.top = '0';
-        div.style.left = '0';
-    };
+    const handleTouchCancel = (e: TouchEvent) => (e.currentTarget as HTMLDivElement).style.top = '0';
     return <Dialog title="学习" onCancel={finish}>
         <div class="relative p-2 h-full flex flex-col bg-cover bg-center text-thick-shadow [outline:none]" tabIndex={-1} onKeyUp={handleKeyPress} onTouchStart={handleTouchStart} onTouchMove={handleTouchMove} onTouchEnd={handleTouchEnd} onTouchCancel={handleTouchCancel} style={(signals.isPhaseAnswer.value && current.value.pic) ? `background-image: url(${current.value.pic});` : ''}>
             <div class="flex gap-2 text-lg">
-                <SButton disabled={index.value <= 0} onClick={handlePrevious}><IconChevronsLeft class="bg-round-6"/></SButton>
                 <div>{index.value+1}/{signals.studies.value.length}</div>
-                <SButton disabled={index.value >= signals.studies.value.length} onClick={handleNext}><IconChevronsRight class="bg-round-6"/></SButton>
+                <SButton onClick={handleShowAnswer} title="_" disabled={signals.isPhaseAnswer.value}><span class="bg-round-6">答</span></SButton>
+                <SButton onClick={handleIKnown} title="X/N" disabled={!signals.isPhaseAnswer.value}><span class="bg-round-6">知</span></SButton>
+                <SButton onClick={handleDontKnow} title="Z/M" disabled={!signals.isPhaseAnswer.value}><span class="bg-round-6">不</span></SButton>
                 <div class="grow"/>
                 {signals.admin.value && <SButton disabled={!signals.isPhaseAnswer.value} onClick={handleDictMaintain}><IconBook2 class="bg-round-6"/></SButton>}
                 <SButton disabled={!shouldSound.value} onClick={handleSpeakIt}><IconPlayerPlayFilled class="bg-round-6"/></SButton>
-                <SButton disabled={!signals.isPhaseAnswer.value} onClick={handleSkilled}><IconCircleLetterF class="bg-round-6"/></SButton>
+                <SButton disabled={!signals.isPhaseAnswer.value} onClick={handleFinished}><IconCircleLetterF class="bg-round-6"/></SButton>
                 <SButton disabled={!signals.isPhaseAnswer.value} onClick={handleDeleteTask}><IconCut class="bg-round-6"/></SButton>
                 <SButton disabled={!signals.isPhaseAnswer.value} onClick={handleReportIssue}><IconAlertCircleFilled class="bg-round-6"/></SButton>
                 <SButton disabled={!signals.isPhaseAnswer.value} onClick={handleRefresh}><IconRefresh class="bg-round-6"/></SButton>
