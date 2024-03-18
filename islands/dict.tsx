00b954f2d5a13ab6a4eaae911a26002115b35a02
sholvoir <sovar.he@gmail.com>
2024-03-17 23:26:21 -0400

    flex

--- islands/dict.tsx
+++ islands/dict.tsx
@@ -20,7 +20,7 @@ export default () => {
     }
     return <Dialog title="词典">
         <div class="p-2 flex gap-2">
-            <TInput name="word" placeholder="word" class="grow" binding={word} onChange={handleSearchClick}/>
+            <TInput name="word" placeholder="word" class="flex-auto" binding={word} onChange={handleSearchClick}/>
             <PButton class="w-20" onClick={handleSearchClick}>查找</PButton>
         </div>
     </Dialog>;
