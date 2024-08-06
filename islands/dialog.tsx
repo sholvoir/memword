f4ccf4d7470c9f29487b5641d4a7d4663001a5bf
sholvoir <sovar.he@gmail.com>
2024-08-06 14:29:16 -0400

    div

--- islands/dialog.tsx
+++ islands/dialog.tsx
@@ -1,15 +1,20 @@
-// deno-lint-ignore-file no-explicit-any
 import { JSX } from "preact";
 import { closeDialog } from '../lib/mem.ts'
 import SButton from '@sholvoir/components/islands/button-base.tsx';
 import IconChevronLeft from "tabler_icons/chevron-left.tsx";
 
-export default ({title, children, onCancel, noback }: {noback?: boolean}&JSX.HTMLAttributes<HTMLDivElement>) => {
+interface IDialogProps {
+    noback?: boolean;
+    onCancel?: () => void;
+}
+export default ({title, children, onCancel, noback }: IDialogProps & JSX.HTMLAttributes<HTMLDivElement>) => {
     return <div class="fixed inset-0 flex flex-col">
-        <div class="shrink-0 px-2 w-[env(titlebar-area-width,100%)] ml-[env(titlebar-area-x,0)] h-[env(titlebar-area-height,38px)] flex justify-between items-center">
-            {noback ? <div class="w-6 h-6"></div> : <SButton class="[app-region:no-drag]" onClick={(onCancel as any) ?? closeDialog}><IconChevronLeft class="w-6 h-6"/></SButton>}
-            <div class="font-bold [app-region:drag]">{title}</div><div class="w-6 h-6"></div>
+        <div class="title shrink-0 px-2 flex justify-between items-center">
+            {noback ? <div class="w-6 h-6"></div> :
+                <SButton class="[app-region:no-drag]" onClick={onCancel ?? closeDialog}><IconChevronLeft class="w-6 h-6"/></SButton>}
+            <div class="font-bold [app-region:drag]">{title}</div>
+            <div class="w-6 h-6"></div>
         </div>
-        <div class="grow overflow-y-auto bg-slate-200 dark:bg-slate-800">{children}</div>
+        <div class="body grow overflow-y-auto ">{children}</div>
     </div>
 }
--- islands/home.tsx
+++ islands/home.tsx
@@ -6,9 +6,9 @@ import IconStudy from "./icon-study.tsx";
 import IconMe from "./icon-me.tsx";
 
 export default () => <div class="fixed inset-0 flex flex-col">
-    <div class="shrink-0 px-2 w-[env(titlebar-area-width,100%)] ml-[env(titlebar-area-x,0)] h-[env(titlebar-area-height,38px)] [app-region:drag] flex justify-center items-center font-bold">学习进度</div>
-    <Stats class="grow bg-slate-200 dark:bg-slate-800 overflow-y-auto"/>
-    <div class="shrink-0 px-4 pt-1 pb-2 bg-slate-300 dark:bg-slate-700 fill-slate-800 dark:fill-slate-300 flex gap-3 justify-between [&>button]:grow">
+    <div class="title [app-region:drag] shrink-0 px-2">学习进度</div>
+    <Stats class="body grow overflow-y-auto"/>
+    <div class="tail shrink-0 px-4 pt-2 pb-4 flex gap-3 justify-between [&>button]:grow">
         <Button onClick={()=>showDialog({dial:'dict'})}><IconDict class="w-9 h-9 inline-block"/>词典</Button>
         <Button onClick={()=>startStudy()}><IconStudy  class="w-9 h-9 inline-block stroke-1"/>学习</Button>
         <Button onClick={()=>showDialog({dial:'menu'})}><IconMe class="w-9 h-9 inline-block stroke-[4] fill-none stroke-slate-800 dark:stroke-slate-300"/>我</Button>
