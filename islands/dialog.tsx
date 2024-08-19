26188bf0c9a00797caf3b3b8f3118b37f0c159df
sholvoir <sovar.he@gmail.com>
2024-08-18 23:49:16 -0400

    tips

--- islands/dialog.tsx
+++ islands/dialog.tsx
@@ -12,11 +12,13 @@ interface IDialogProps {
 export default ({title, children, onCancel, noback, onMenuClick }: IDialogProps & JSX.HTMLAttributes<HTMLDivElement>) => {
     return <div class="fixed inset-0 flex flex-col">
         <div class="title shrink-0 px-2 flex justify-between items-center">
-            {noback ? <div class="w-6 h-6"></div> :
-                <SButton class="[app-region:no-drag]" onClick={onCancel ?? closeDialog}><IconChevronLeft class="w-6 h-6"/></SButton>}
-            <div class="font-bold [app-region:drag]">{title}</div>
-            {!onMenuClick ? <div class="w-6 h-6"></div> :
-                <SButton class="[app-region:no-drag]" onClick={onMenuClick}><IconDots class="w-6 h-6"/></SButton>}
+            <div class="w-6">
+                {!noback && <SButton class="[app-region:no-drag]" onClick={onCancel ?? closeDialog}><IconChevronLeft class="w-6 h-6"/></SButton>}
+            </div>
+            <div class="grow font-bold text-center [app-region:drag]">{title}</div>
+            <div class="w-6">
+                {!onMenuClick && <SButton class="[app-region:no-drag]" onClick={onMenuClick}><IconDots class="w-6 h-6"/></SButton>}
+            </div>
         </div>
         <div class="body grow overflow-y-auto ">{children}</div>
     </div>
