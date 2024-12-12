d6f8a81576721953d0fd83085219f331ed915baf
sholvoir <sovar.he@gmail.com>
2024-12-11 22:17:00 -0500

    0.2.4

--- islands/study.tsx
+++ islands/study.tsx
@@ -18,7 +18,7 @@ import Dialog from './dialog.tsx';
 
 const audioUrl = (sound?: string) => {
     if (!sound) return undefined;
-    if (sound.startsWith("data:")) return sound;
+    if (sound.startsWith("data")) return sound;
     if (sound.startsWith("http")) return `${DICT_API}/pub/sound?q=${encodeURIComponent(sound)}`;
 }
 
@@ -126,42 +126,44 @@ export default () => {
     }, []);
     return <Dialog title="学习" onCancel={finish}>
         <div class={`relative h-full [outline:none]`} tabIndex={-1} style={`top: ${endY.value - startY.value}px`}>
-            <div class="h-full study-translucent flex bg-cover bg-center flex-col"
-                style={(signals.isPhaseAnswer.value && dict.value?.pic) ? `background-image: url(${dict.value.pic});` : ''}>
-                <div class="shrink-0 p-2 flex gap-2 text-lg">
-                    <SButton disabled={!signals.isPhaseAnswer.value} onClick={()=>handleIKnown()} title="X/N">
-                        <IconCheck class="bg-round-6"/>
-                    </SButton>
-                    <SButton disabled={!signals.isPhaseAnswer.value} onClick={()=>handleIKnown(0)} title="Z/M">
-                        <IconX class="bg-round-6"/>
-                    </SButton>
-                    <SButton disabled={!signals.isPhaseAnswer.value} onClick={handleSpeakIt}>
-                        <IconPlayerPlayFilled class="bg-round-6"/>
-                    </SButton>
-                    <div class="grow text-center">{index.value+1}/{signals.tasks.value.length}</div>
-                    <SButton disabled={!signals.isPhaseAnswer.value} onClick={()=>handleIKnown(13)}>
-                        <IconCircleLetterF class="bg-round-6"/>
-                    </SButton>
-                    <SButton disabled={!signals.isPhaseAnswer.value} onClick={handleDeleteTask}>
-                        <IconCut class="bg-round-6"/>
-                    </SButton>
-                    <SButton disabled={!signals.isPhaseAnswer.value} onClick={handleReportIssue}>
-                        <IconAlertCircleFilled class="bg-round-6"/>
-                    </SButton>
-                    <SButton disabled={!signals.isPhaseAnswer.value} onClick={handleRefresh}>
-                        <IconRefresh class="bg-round-6"/>
-                    </SButton>
-                    <div>{current.value.level}</div>
-                </div>
-                <div class="grow px-2 h-full" onTouchStart={handleTouchStart} onTouchMove={handleTouchMove} onTouchEnd={handleTouchEnd} onTouchCancel={handleTouchCancel} onClick={handleClick}>
-                    <div class="pb-2 flex gap-2 flex-wrap justify-between">
-                        {splite(current.value.word)}
-                        {signals.isPhaseAnswer.value && <div class="text-2xl flex items-center">{dict.value?.phonetic}</div>}
+            <div class="h-full bg-cover bg-center" style={(signals.isPhaseAnswer.value && dict.value?.pic) ? `background-image: url(${dict.value.pic});` : ''}>
+                <div class="h-full study-translucent flex flex-col">
+                    <div class="shrink-0 p-2 flex gap-2 text-lg">
+                        <SButton disabled={!signals.isPhaseAnswer.value} onClick={()=>handleIKnown()} title="X/N">
+                            <IconCheck class="bg-round-6"/>
+                        </SButton>
+                        <SButton disabled={!signals.isPhaseAnswer.value} onClick={()=>handleIKnown(0)} title="Z/M">
+                            <IconX class="bg-round-6"/>
+                        </SButton>
+                        <SButton disabled={!signals.isPhaseAnswer.value} onClick={handleSpeakIt}>
+                            <IconPlayerPlayFilled class="bg-round-6"/>
+                        </SButton>
+                        <div class="grow text-center">{index.value+1}/{signals.tasks.value.length}</div>
+                        <SButton disabled={!signals.isPhaseAnswer.value} onClick={()=>handleIKnown(13)}>
+                            <IconCircleLetterF class="bg-round-6"/>
+                        </SButton>
+                        <SButton disabled={!signals.isPhaseAnswer.value} onClick={handleDeleteTask}>
+                            <IconCut class="bg-round-6"/>
+                        </SButton>
+                        <SButton disabled={!signals.isPhaseAnswer.value} onClick={handleReportIssue}>
+                            <IconAlertCircleFilled class="bg-round-6"/>
+                        </SButton>
+                        <SButton disabled={!signals.isPhaseAnswer.value} onClick={handleRefresh}>
+                            <IconRefresh class="bg-round-6"/>
+                        </SButton>
+                        <div>{current.value.level}</div>
                     </div>
-                    {signals.isPhaseAnswer.value && <div>
-                        {dict.value?.trans?.split('\n').map((t: string) => <p class="text-2xl">{t}</p>)}
-                        {dict.value?.def?.split('\n').map((t: string) => t.startsWith(' ')?<p class="text-lg">&ensp;&bull;{t}</p>:<p class="text-xl font-bold">{t}</p>)}
-                    </div>}
+                    <div class="grow px-2 h-full" onTouchStart={handleTouchStart} onTouchMove={handleTouchMove}
+                        onTouchEnd={handleTouchEnd} onTouchCancel={handleTouchCancel} onClick={handleClick}>
+                        <div class="pb-2 flex gap-2 flex-wrap justify-between">
+                            {splite(current.value.word)}
+                            {signals.isPhaseAnswer.value && <div class="text-2xl flex items-center">{dict.value?.phonetic}</div>}
+                        </div>
+                        {signals.isPhaseAnswer.value && <div>
+                            {dict.value?.trans?.split('\n').map((t: string) => <p class="text-2xl">{t}</p>)}
+                            {dict.value?.def?.split('\n').map((t: string) => t.startsWith(' ')?<p class="text-lg">&ensp;&bull;{t}</p>:<p class="text-xl font-bold">{t}</p>)}
+                        </div>}
+                    </div>
                 </div>
             </div>
         </div>
