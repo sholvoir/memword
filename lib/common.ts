0266ebdf98032fb3fd2db243e61528bbd6bd6247
sholvoir <sovar.he@gmail.com>
2025-01-11 16:06:40 -0500

    0.3.26

--- lib/common.ts
+++ lib/common.ts
@@ -1,3 +1,3 @@
-export const VOCABULARY_URL = 'https://www.micit.co/vocabulary/vocabulary-0.1.8.txt';
+export const VOCABULARY_URL = 'https://www.micit.co/vocabulary/vocabulary-0.1.9.txt';
 export const DICT_API = 'https://dict.micit.co';
 export const now = () => Math.floor(Date.now() / 1000);
