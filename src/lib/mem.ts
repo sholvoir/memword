c7eb9bb3c2b5beffb0ed9154622ac373fc386495
sholvoir <sovar.he@gmail.com>
2026-04-20 09:11:16 -0400

    0.24.17

--- src/lib/mem.ts
+++ src/lib/mem.ts
@@ -1,4 +1,3 @@
-import type { IDict } from "@sholvoir/dict-server/src/lib/imic.ts";
 import { blobToBase64 } from "@sholvoir/generic/blob";
 import { STATUS_CODE } from "@sholvoir/generic/http";
 import { parse } from "yaml";
@@ -33,21 +32,9 @@ export const syncSetting = async (cSetting?: ISetting) => {
    } catch {}
 };
 
-const ecdictIssue = (dict: IDict) => {
-   if (dict.entries)
-      A: for (const entry of dict.entries)
-         if (entry.meanings)
-            for (const pos of Object.keys(entry.meanings))
-               if (pos === "ecdict") {
-                  submitIssue(dict.word, "1");
-                  break A;
-               }
-};
-
 export const updateDict = async (item: IItem) => {
    const dict = await srv.getDict(item.word);
    if (!dict) return item;
-   ecdictIssue(dict);
    if (
       item.version !== undefined &&
       dict.version !== undefined &&
@@ -78,7 +65,6 @@ export const search = async (word: string) => {
       try {
          const dict = await srv.getDict(word);
          if (!dict) return;
-         ecdictIssue(dict);
          const nitem = newItem(dict);
          idb.tempItems.set(word, nitem);
          return nitem;
